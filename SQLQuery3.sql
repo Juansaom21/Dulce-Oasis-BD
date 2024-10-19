1bef8c3496e9497874adac7684d3fff537c6a4ea
Juan Miguel <juan.guisao614@pascualbravo.edu.co>
2024-10-19 18:08:05 -0500

    Segundo commit del proyecto

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -2,26 +2,26 @@ create database Dulce_Oasis
 use Dulce_Oasis
 
 create table producto(
-cod_prod int not null primary key,
+cod_prod int identity(1,1) not null primary key,
 nombre_prod varchar(50) not null,
 cantidad_prod int not null,
 descrip varchar(50),
 precio bigint not null,)
 
 create table ingrediente(
-cod_ingr int not null primary key,
+cod_ingr int identity(1,1) not null primary key,
 nombre_ingr varchar(50) not null,
 cantidad_ingr int not null)
 
 create table usuario(
-id int not null primary key,
+id int identity(1,1) not null primary key,
 nombre varchar(50) not null,
 correo varchar(50) not null,
 contra varchar(50) not null,
 rol varchar(50) not null check(rol in('admin', 'empleado')))
 
 create table factura(
-cod_factura int not null primary key,
+cod_factura int identity(1,1) not null primary key,
 id int not null foreign key references usuario(id),
 cod_prod int not null foreign key references producto(cod_prod),
 fecha date not null,
